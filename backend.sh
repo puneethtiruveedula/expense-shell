44f8d776dc02495a39a93f2881b67d2147ab26be
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-05-02 23:55:05 +0530

    Sql-installation

--- backend.sh
+++ backend.sh
@@ -65,3 +65,23 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies
 
 #check your repo and path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h 172.31.30.192 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
